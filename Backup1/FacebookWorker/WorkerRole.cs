51fc186f9dd26751dc36221db6bdec330ee64993
Thomas Conte <thomas@conte.com>
2011-09-19 15:43:53 +0200

    Updated: Azure SDK 1.5, Facebook SDK 5.2.1.0

---
+++ Backup1/FacebookWorker/WorkerRole.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+using LikerLib;
+
+namespace FacebookWorker
+{
+    public class WorkerRole : RoleEntryPoint
+    {
+        public override void Run()
+        {
+            // This is a sample worker implementation. Replace with your logic.
+            Trace.WriteLine("FacebookWorker entry point called", "Information");
+
+            var account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
+
+            var queueClient = account.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("likesqueue");
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+                Trace.WriteLine("Working", "Information");
+
+                var msg = queue.GetMessage(TimeSpan.FromMinutes(5)); // Let's give us five minutes to process
+                if (msg != null)
+                {
+                    var p = msg.AsString.Split('%');
+                    Trace.WriteLine("Processing " + p[0], "Information");
+                    // TODO: start new thread
+                    var service = new FriendLikesService(p[0], p[1]);
+                    // TODO: handle refreshes!
+                    if (service.isCached())
+                    {
+                        Trace.WriteLine("Already in cache, skipping " + p[0], "Information");
+                    }
+                    else
+                    {
+                        var watch = new Stopwatch();
+                        watch.Start();
+                        service.GetFriendsLikes();
+                        service.SaveFriendLikes();
+                        watch.Stop();
+                        Trace.WriteLine("Done with " + p[0] + " time: " + watch.Elapsed, "Information");
+                    }
+                    queue.DeleteMessage(msg);
+                }
+
+            }
+        }
+
+        public override bool OnStart()
+        {
+            // Set the maximum number of concurrent connections 
+            ServicePointManager.DefaultConnectionLimit = 12;
+
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            var account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
+
+            var queueClient = account.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("likesqueue");
+            queue.CreateIfNotExist();
+
+            return base.OnStart();
+        }
+    }
+}
---
+++ Backup1/LikerLib/FriendLikesService.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Facebook;
+using Facebook.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.StorageClient;
+using Newtonsoft.Json;
+
+namespace LikerLib
+{
+    public class FriendLikesService
+    {
+        private const string FRIEND_LIKES_TABLE = "friendlikes";
+        private const string FRIEND_LIKES_BLOB_CONTAINER = "friendlikes";
+        public String AccessToken { get; set; }
+        public String UserId { get; set; }
+        public Dictionary<string, FriendLike> FriendLikes { get; set; }
+        private CloudStorageAccount account;
+        private CloudTableClient tableClient;
+        private CloudBlobClient blobClient;
+        private CloudBlobContainer blobContainer;
+
+        public FriendLikesService(string id, String token)
+        {
+            this.AccessToken = token;
+            this.UserId = id;
+            account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
+            // Create the Table client
+            tableClient = new CloudTableClient(account.TableEndpoint.ToString(), account.Credentials);
+
+            // Create the Blob Container
+            blobClient = new CloudBlobClient(account.BlobEndpoint.ToString(), account.Credentials); 
+            blobContainer = blobClient.GetContainerReference(FRIEND_LIKES_BLOB_CONTAINER);
+            blobContainer.CreateIfNotExist(); // TODO: MOVE THIS OUT?
+            blobContainer.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+        }
+
+        // Lightweight (no access token) constructor, used by the JSON controller only
+        public FriendLikesService(string id)
+        {
+            this.UserId = id;
+            account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("DataConnectionString"));
+            tableClient = new CloudTableClient(account.TableEndpoint.ToString(), account.Credentials);
+
+            // Create the Blob Container
+            blobClient = new CloudBlobClient(account.BlobEndpoint.ToString(), account.Credentials);
+            blobContainer = blobClient.GetContainerReference(FRIEND_LIKES_BLOB_CONTAINER);
+            blobContainer.CreateIfNotExist(); // TODO: MOVE THIS OUT?
+            blobContainer.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+        }
+
+        public void GetFriendsLikes()
+        {
+            // Find all friend likes & store in Dictionary
+
+            // Create the table if necessary
+            tableClient.CreateTableIfNotExist(FRIEND_LIKES_TABLE); // MOVE THIS OUT?
+
+            // If we have previously cached the friend likes for this user, use them
+            // TODO: we probably need a way to refresh the data from time to time
+            // TODO: test existence of the response BLOB
+            // TODO: refresh if blob older than n days/hours?
+
+            if (isCached())
+            {
+                var query = tableClient.
+                    GetDataServiceContext().
+                    CreateQuery<FriendLike>(FRIEND_LIKES_TABLE);
+
+                var likes = (from l in query where l.PartitionKey == UserId select l).AsTableServiceQuery();
+
+                FriendLikes = likes.ToDictionary(l => l.RowKey);
+            }
+            else
+            {
+                FriendLikes = new Dictionary<string, FriendLike>();
+
+                // Let's use the Facebook Graph API to gather all the info
+                var fb = new FacebookClient(AccessToken);
+                // TODO: if the user logged out of Facebook in the meantime, he access token could be invalid and we will receive an exception!
+                dynamic friends = fb.Get("me/friends");
+                foreach (var f in friends.data)
+                {
+                    dynamic likes = fb.Get(f.id + "/likes"); // TODO: exception handling
+                    foreach (var l in likes.data)
+                    {
+                        if (FriendLikes.ContainsKey(l.id))
+                        {
+                            ++FriendLikes[l.id].Nb;
+                        }
+                        else
+                        {
+                            // Clean up the category field
+                            var category = l.category.ToLower().Replace('/', '-').Replace(' ', '-');
+                            FriendLikes[l.id] = new FriendLike(UserId, l.id, l.name, category, l.picture);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SaveFriendLikes()
+        {
+            // Persist friend likes to Table Storage
+
+            var context = tableClient.GetDataServiceContext();
+
+            // TODO: for refreshes, need to handle updating the entities (or replacing)
+            var n = 0;
+            foreach (var k in FriendLikes.Keys)
+            {
+                context.AddObject(FRIEND_LIKES_TABLE, FriendLikes[k]);
+                if (n++ % 100 == 0)
+                {
+                    context.SaveChangesWithRetries(System.Data.Services.Client.SaveChangesOptions.Batch);
+                }
+            }
+
+            // Create the pre-calculated JSON Blob
+
+            var blob = blobContainer.GetBlobReference(UserId);
+            var likes = GetOrderedFriendLikes();
+            var json = JsonConvert.SerializeObject(likes);
+            blob.UploadText("dataCallback(" + json + ")");
+        }
+
+        public bool isCached()
+        {
+            var blob = blobContainer.GetBlobReference(UserId);
+
+            try
+            {
+                blob.FetchAttributes();
+            }
+            catch (StorageClientException ex)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public List<FriendLike> GetOrderedFriendLikes()
+        {
+            var orderedLikes = new List<FriendLike>();
+
+            // Sort friend likes by descending number of likes
+            // Strip everything that does not have at least 2 likes
+
+            foreach (var l in FriendLikes.Where(k => k.Value.Nb > 1).OrderByDescending(k => k.Value.Nb))
+            {
+                orderedLikes.Add(l.Value);
+            }
+
+            return orderedLikes;
+        }
+
+        public Dictionary<string, int> GetCategories()
+        {
+            var categories = new Dictionary<string, int>();
+
+            foreach (var k in FriendLikes)
+            {
+                var v = k.Value.Category;
+                if (!String.IsNullOrEmpty(v))
+                {
+                    if (categories.ContainsKey(v))
+                    {
+                        categories[v]++;
+                    }
+                    else
+                    {
+                        categories[v] = 1;
+                    }
+                }
+            }
+
+            return categories;
+        }
+
+        public void QueueLike()
+        {
+            var queueClient = account.CreateCloudQueueClient();
+            var queue = queueClient.GetQueueReference("likesqueue");
+            queue.CreateIfNotExist(); // MOVE THIS OUT
+            var msg = new CloudQueueMessage(UserId + "%" + AccessToken);
+            queue.AddMessage(msg);
+        }
+    }
+}
